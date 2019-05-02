75893b1db0582a80df35fcce46684deb0e9a6101
Alex Cooper <alexcooper122311@gmail.com>
2019-05-01 20:49:50 -0400

    New Email
    Changed the email address at the bottom to my current one, and also cleaned up the javascript file.

--- scripts/index.js
+++ scripts/index.js
@@ -6,18 +6,15 @@ let cur = [];
 
 window.addEventListener("scroll", event => {
     let fromTop = window.scrollY;
-  
     mainNavLinks.forEach(link => {
-      let section = document.querySelector(link.hash);
-  
-      if (
-        section.offsetTop <= fromTop &&
-        section.offsetTop + section.offsetHeight > fromTop
-      ) {
-        link.classList.add("current");
-      } else {
-        link.classList.remove("current");
-      }
+        let section = document.querySelector(link.hash);
+        if (
+            section.offsetTop <= fromTop &&
+            section.offsetTop + section.offsetHeight > fromTop
+        ) {
+            link.classList.add("current");
+        } else {
+            link.classList.remove("current");
+        }
     });
-  });
-  
+});
